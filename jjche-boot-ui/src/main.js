bc39d817c6b86ae8cc3fef4f570ad3a0970fce96
miaoyinjun <312656362@qq.com>
2023-03-10 13:33:01 +0800

    feat(前端.优化): 同步eladmin.2.6
    打包优化

--- jjche-boot-ui/src/main.js
+++ jjche-boot-ui/src/main.js
@@ -5,9 +5,6 @@ import Cookies from 'js-cookie'
 import 'normalize.css/normalize.css'
 
 import Element from 'element-ui'
-//
-import mavonEditor from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
 
 // 数据字典
 import dict from './components/Dict'
@@ -18,20 +15,13 @@ import './assets/styles/element-variables.scss'
 // global css
 import './assets/styles/index.scss'
 
-// 代码高亮
-import VueHighlightJS from 'vue-highlightjs'
-import 'highlight.js/styles/atom-one-dark.css'
-
 import App from './App'
 import store from './store'
 import router from './router/routers'
 
 import './assets/icons' // icon
 import './router/index' // permission control
-import 'echarts-gl'
 
-Vue.use(VueHighlightJS)
-Vue.use(mavonEditor)
 Vue.use(permission)
 Vue.use(dict)
 Vue.use(Element, {
--- jjche-boot-ui/src/views/components/MarkDown.vue
+++ jjche-boot-ui/src/views/components/MarkDown.vue
@@ -1,29 +1,34 @@
 <template>
-  <div class="app-container">
-    <p class="warn-content">
+  <div className="app-container">
+    <p className="warn-content">
       Markdown 基于
-      <el-link
-        type="primary"
-        href="https://github.com/hinesboy/mavonEditor"
-        target="_blank"
-      >MavonEditor</el-link>
+      <el-link type="primary" href="https://github.com/hinesboy/mavonEditor" target="_blank">MavonEditor</el-link>
     </p>
-    <mavon-editor ref="md" :style="'height:' + height" @imgAdd="imgAdd" />
+    <mavon-editor ref="md" :style="'height:' + height" @imgAdd="imgAdd"/>
   </div>
 </template>
 
 <script>
 import { upload } from '@/utils/upload'
 import { mapGetters } from 'vuex'
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
 export default {
   name: 'Markdown',
+  components: {
+    mavonEditor
+  },
   data() {
     return {
       height: document.documentElement.clientHeight - 200 + 'px'
     }
   },
   computed: {
-    ...mapGetters(['imagesUploadApi', 'baseApi'])
+    ...mapGetters([
+      'imagesUploadApi',
+      'baseApi'
+    ])
   },
   mounted() {
     const that = this
@@ -33,8 +38,9 @@ export default {
   },
   methods: {
     imgAdd(pos, $file) {
-      upload(this.imagesUploadApi, $file).then((res) => {
-        const url = res.data.data[0].path
+      upload(this.imagesUploadApi, $file).then(res => {
+        const data = res.data
+        const url = this.baseApi + '/file/' + data.type + '/' + data.realName
         this.$refs.md.$img2Url(pos, url)
       })
     }
